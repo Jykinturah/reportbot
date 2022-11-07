f42fcceef1ab05709cf61ff6be19bbb2b6cb8f0f
Jykinturah Sparkle <jykinturah@gmail.com>
2022-11-06 18:36:48 -0800

    Add presence

--- events/ready.js
+++ events/ready.js
@@ -4,10 +4,16 @@
  * client.once('ready',(client) => {code});
 */
 
+const { ActivityType } = require('discord.js');
+
 module.exports = {
   name: 'ready',
   once: true,
   async execute(client){
-    console.log('Reportbot Ready!');
+    console.log(`Reportbot running as ${client.user.tag}`);
+    client.user.setPresence({
+      activities: [{ name: `reports`, type: ActivityType.Listening}]
+    });
+    console.log("https://discordapp.com/oauth2/authorize?client_id="+client.user.id+"&scope=bot");
   }
 }
